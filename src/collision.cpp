5b2bad6f1a8a14821f49e11231974ad252afd8a6
b1becker <128233074+b1becker@users.noreply.github.com>
2025-11-14 12:17:41 -0500

    impl of circular buffer in collision

--- src/collision.cpp
+++ src/collision.cpp
@@ -1,57 +1,105 @@
 #include <Arduino.h>
 #include "collision.h"
 
+using namespace std;
+
 #define STD_DELAY 50
 #define N_SAMPLES 10
 #define DISTANCE_THRESHOLD 250
 
+
+
+
 collision::collision(int photodiode_pin, int irLED_pin) {
     photodiode = photodiode_pin;
     irLED = irLED_pin;
+
+    curr_buffer_index = 0;
 }
+// have to use standard for loop since low memory
+
 
 void collision::setup() {
     pinMode(photodiode, INPUT);
     pinMode(irLED, OUTPUT);
+    
+    // Initialize buffers with N_SAMPLES readings using for loop
+    Serial.println("Initializing collision sensor buffers...");
+    
+    for (int i = 0; i < N_SAMPLES; i++) {
+        // irLED low and sample OFF
+        digitalWrite(irLED, LOW);
+        delay(STD_DELAY);
+        offBuffer[i] = analogRead(photodiode);
+        offAVG += offBuffer[i];
+        
+        // irLED high and sample ON
+        digitalWrite(irLED, HIGH);
+        delay(STD_DELAY);
+        onBuffer[i] = analogRead(photodiode);
+        onAVG += onBuffer[i];
+    }
+    
+    // Calculate initial averages
+    offAVG /= N_SAMPLES;
+    onAVG /= N_SAMPLES;
+    
+    Serial.print("Initial offAVG: ");
+    Serial.println(offAVG);
+    Serial.print("Initial onAVG: ");
+    Serial.println(onAVG);
 }
 
 int collision::loop(bool* wall) {
-
-    float onSUM = 0;
-    float offSUM = 0; 
-
+    // Get old values that will be replaced
+    int curr_off_buffer = offBuffer[curr_buffer_index];
+    int curr_on_buffer = onBuffer[curr_buffer_index];
+    
     // irLED low and sample
     digitalWrite(irLED, LOW);
     delay(STD_DELAY);
-    for (int i = 0; i < N_SAMPLES; i++) {
-        offSUM += analogRead(photodiode);
-    }
-    int offAVG = offSUM / N_SAMPLES;
+    
+    int curr_off_read = analogRead(photodiode);
+    offBuffer[curr_buffer_index] = curr_off_read;
+    
+    // Update running average
+    offAVG -= curr_off_buffer / N_SAMPLES;
+    offAVG += curr_off_read / N_SAMPLES;
+    
+    Serial.print("offAVG: ");
     Serial.println(offAVG);
     delay(STD_DELAY);
-
+    
     // irLED high and sample
     digitalWrite(irLED, HIGH);
     delay(STD_DELAY);
-        for (int i = 0; i < N_SAMPLES; i++) {
-        onSUM += analogRead(photodiode);
-    }
-    int onAVG = onSUM / N_SAMPLES;
+    
+    int curr_on_read = analogRead(photodiode);
+    onBuffer[curr_buffer_index] = curr_on_read;
+    
+    // Update running average
+    onAVG -= curr_on_buffer / N_SAMPLES;
+    onAVG += curr_on_read / N_SAMPLES;
+    
+    Serial.print("onAVG: ");
     Serial.println(onAVG);
     delay(STD_DELAY);
-
+    
+    // Advance circular buffer index with wraparound
+    curr_buffer_index = (curr_buffer_index + 1) % N_SAMPLES;
+    
     // reflection with ambient - ambient = just reflected
     int reflected = onAVG - offAVG;
-
+    
     if (reflected > DISTANCE_THRESHOLD) {
-        Serial.print("Collition approaching! Reflected = ");
+        Serial.print("Collision approaching! Reflected = ");
         Serial.println(reflected);
         *wall = true;
-
     } else {
         Serial.print("Continue with clear path. Reflected = ");
         Serial.println(reflected);
         *wall = false;
     }
+    
     return reflected;
 }
--- src/collision.h
+++ src/collision.h
@@ -1,6 +1,8 @@
 #ifndef COLLISION_H
 #define COLLISION_H
 
+#define N_SAMPLES 10
+
 class collision
 {
 private:
@@ -11,6 +13,18 @@ public:
     int loop(bool* wall);
     void setup();
     collision(int photodiode_pin, int irLED_pin);
+
+private:
+
+    int offBuffer[N_SAMPLES];
+    int onBuffer[N_SAMPLES];
+    int curr_buffer_index;
+
+    int offAVG;
+    int onAVG;
+
+    int arraySum(int arr[], int size);
+
 };
 
 
